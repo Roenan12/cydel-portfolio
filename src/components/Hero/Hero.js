6bbacc7169a5dca5a3151bc43a6ffcb0dd94b8dd
Roenan Halili <haliliroenan12@gmail.com>
2024-07-29 15:08:18 +0800

    add loader to hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import garage from "../../assets/3-done.jpg";
 import "./Hero.css";
+import { Loader } from "../Loader";
 
-export function Hero() {
+export function Hero({ isLoading, setIsLoading }) {
+  useEffect(() => {
+    const img = new Image();
+    img.src = garage;
+    if (img.complete) {
+      setIsLoading(false);
+    } else {
+      setIsLoading(true);
+      img.onload = () => setIsLoading(false);
+    }
+  }, [setIsLoading]);
+
   return (
     <>
       <section id="home" className="hero">
-        <div className="hero-content">
-          <h2>Cydel Iron Works</h2>
-          <p>Your Roll up Door Specialist</p>
+        {isLoading ? (
+          ""
+        ) : (
+          <div className="hero-content">
+            <h2>Cydel Iron Works</h2>
+            <p>Your Roll up Door Specialist</p>
 
-          <a className="hero-btn" href="#contact">
-            Inquire Now
-          </a>
-        </div>
-        <img className="hero-img kenburns-top" src={garage} alt="garage" />
+            <a className="hero-btn" href="#contact">
+              Inquire Now
+            </a>
+          </div>
+        )}
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <img
+            className="hero-img kenburns-top"
+            src={garage}
+            alt="hero roll up door"
+          />
+        )}
         <div className="hero-overlay"></div>
         <div className="wave">
           <svg
