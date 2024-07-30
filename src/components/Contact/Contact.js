7859a03b27f1e2bd7e23959aa56c28dd3a2aa152
Roenan Halili <haliliroenan12@gmail.com>
2024-07-30 16:17:59 +0800

    Add contact form functionality with recaptcha

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,53 @@
 import React, { useState } from "react";
 import { contacts } from "../../constants/index";
 import "./Contact.css";
+import axios from "axios";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export function Contact() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [capVal, setCapVal] = useState(null);
+
+  const SITE_KEY = process.env.REACT_APP_SITE_KEY;
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY;
+    const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+    const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+
+    //dynamic template object
+    const data = {
+      service_id: SERVICE_ID,
+      template_id: TEMPLATE_ID,
+      user_id: PUBLIC_KEY,
+      template_params: {
+        from_name: name,
+        from_email: email,
+        to_name: "Cydel",
+        message: message,
+      },
+    };
+
+    //Send email via axios
+    try {
+      const res = await axios.post(
+        "https://api.emailjs.com/api/v1.0/email/send",
+        data
+      );
+      alert("Your mail is sent!");
+      console.log(res.data);
+      setName("");
+      setEmail("");
+      setMessage("");
+    } catch (error) {
+      alert("Oops... " + JSON.stringify(error));
+      console.log(error);
+    }
+  }
 
   return (
     <>
@@ -17,7 +59,7 @@ export function Contact() {
         </div>
         <div className="wrapper">
           <div className="contact-container">
-            <form className="contact-form">
+            <form className="contact-form" onSubmit={handleSubmit}>
               <p>Name</p>
               <input
                 type="text"
@@ -39,7 +81,11 @@ export function Contact() {
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
               />
-              <button type="submit" className="send-button">
+              <ReCAPTCHA
+                sitekey={SITE_KEY}
+                onChange={(val) => setCapVal(val)}
+              />
+              <button type="submit" className="send-button" disabled={!capVal}>
                 Send Email
               </button>
             </form>
