8632440bc75abd42ca69c976782af075dc3f9d33
Roenan Halili <haliliroenan12@gmail.com>
2024-07-30 16:18:19 +0800

    code cleanup

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -102,16 +102,6 @@ export function Contact() {
             ))}
           </div>
         </div>
-        {/* <div>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d467.98329691217083!2d120.69789111962491!3d15.016560475431648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3396f7852cd0ec5f%3A0x8d530e10e17561d!2sCydel%20Iron%20Works!5e0!3m2!1sen!2sph!4v1722074522061!5m2!1sen!2sph"
-            width="600"
-            height="450"
-            allowfullscreen="true"
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </div> */}
       </section>
     </>
   );
