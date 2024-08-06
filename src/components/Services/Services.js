47cdbe21beacfefec1e60ebf38ee729acd0b143a
Roenan Halili <haliliroenan12@gmail.com>
2024-08-07 00:04:05 +0800

    updated section with title component

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,15 +1,25 @@
 import "./Services.css";
+import { TitleHeader } from "../TitleHeader";
 import { Card } from "./Card.js";
 
 export function Services({ theme, onsetTheme }) {
   return (
     <>
-      <h1 className="services" id="services">
-        What We Offer
-      </h1>
-      <section className="card-container">
-        <Card />
+      <TitleHeader
+        title="What We Offer"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        id="services"
+      />
+      <section>
+        <div className="card-container">
+          <Card />
+        </div>
       </section>
+      <TitleHeader
+        title="Why Choose Us"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        id="services"
+      />
     </>
   );
 }
