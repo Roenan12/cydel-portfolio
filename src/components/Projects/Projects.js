b964ce8a276fded7e03e19238b14af07aec459db
Roenan Halili <haliliroenan12@gmail.com>
2024-08-07 00:04:05 +0800

    updated section with title component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,27 +1,35 @@
 import React from "react";
 import "./Projects.css";
+import { TitleHeader } from "../TitleHeader";
 import { images } from "../../constants/index";
 
 export function Projects() {
   return (
     <>
-      <h1 className="project-header" id="projects">
-        Some of our Projects
-      </h1>
-      <section className="slider-container">
-        <div className="slider">
-          <div className="slide-track">
-            {images.map((items, index) => (
-              <div className="slide" key={index}>
-                <img src={items.photo} alt="installed roll up doors"></img>
-              </div>
-            ))}
+      <TitleHeader
+        title="Our Remarkable Project Portfolio"
+        description=" Explore our extensive portfolio showcasing the breadth of our expertise,
+        from iconic shutter doors to sustainable solutions, all meticulously
+        crafted to perfection"
+        id="projects"
+      />
 
-            {images.map((items, index) => (
-              <div className="slide" key={index}>
-                <img src={items.photo} alt="installed roll up doors"></img>
-              </div>
-            ))}
+      <section className="portfolio-container">
+        <div className="slider-container">
+          <div className="slider">
+            <div className="slide-track">
+              {images.map((items, index) => (
+                <div className="slide" key={index}>
+                  <img src={items.photo} alt="installed roll up doors"></img>
+                </div>
+              ))}
+
+              {images.map((items, index) => (
+                <div className="slide" key={index}>
+                  <img src={items.photo} alt="installed roll up doors"></img>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </section>
