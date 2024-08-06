de1c60b352d61bbab65edfcc974ca1681d52abae
Roenan Halili <haliliroenan12@gmail.com>
2024-08-06 23:59:08 +0800

    remove dark theme

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Navbar } from "./components/Header/Navbar.js";
 import { Hero } from "./components/Hero/Hero.js";
+import { About } from "./components/About/About.js";
 import { Services } from "./components/Services/Services.js";
 import { Projects } from "./components/Projects/Projects.js";
 import { Contact } from "./components/Contact/Contact.js";
 import { Footer } from "./components/Footer/Footer.js";
 
 function App() {
-  const current_theme = localStorage.getItem("current_theme");
-  const [theme, setTheme] = useState(current_theme ? current_theme : "light");
-
   const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    localStorage.setItem("current_theme", theme);
-  }, [theme]);
 
   return (
     <>
-      <div className={`container ${theme}`}>
-        <Navbar theme={theme} onSetTheme={setTheme} />
+      <div className="container">
+        <Navbar />
         <Hero isLoading={isLoading} setIsLoading={setIsLoading} />
+        <About />
         <Services />
         <Projects />
-        <Contact
-          isLoading={isLoading}
-          setIsLoading={setIsLoading}
-          theme={theme}
-        />
+        <Contact isLoading={isLoading} setIsLoading={setIsLoading} />
         <Footer />
       </div>
     </>
